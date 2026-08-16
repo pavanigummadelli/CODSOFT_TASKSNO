265c8fa78b4b074147d767ad29ee96eb7cef88e0
G.PAVANI <pavanigummadelli@gmail.com>
2026-08-16 23:35:42 +0530

    Task 1 - Number Game

---
+++ NumberGame.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class NumberGame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        int numberToGuess = rand.nextInt(100) + 1; // random number 1–100
+        int guess = 0;
+        int attempts = 0;
+
+        System.out.println("Guess the number between 1 and 100:");
+
+        while (guess != numberToGuess) {
+            System.out.print("Enter your guess: ");
+            guess = sc.nextInt();
+            attempts++;
+
+            if (guess < numberToGuess) {
+                System.out.println("Too low!");
+            } else if (guess > numberToGuess) {
+                System.out.println("Too high!");
+            } else {
+                System.out.println("Correct! You guessed it in " + attempts + " attempts.");
+            }
+        }
+        sc.close();
+    }
+}
+
+
